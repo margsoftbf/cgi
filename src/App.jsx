9a7ff2569689415fd36b406faa2ba51daa68fd63
Marcin <mgarski@tutanota.com>
2024-10-06 15:39:40 +0200

    add another feat

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Slider1 from './components/Slider1';
 import Slider2 from './components/Slider2';
 import Slider3 from './components/Slider3';
 import Slider4 from './components/Slider4';
+import About from './components/About';
 function App() {
 	const [activeSlider, setActiveSlider] = useState('slider1');
 
@@ -13,15 +14,12 @@ function App() {
 	};
 	return (
 		<>
-			<Navbar onSliderChange={handleSliderChange}/>
+			<Navbar onSliderChange={handleSliderChange} />
 			{activeSlider === 'slider1' && <Slider1 />}
 			{activeSlider === 'slider2' && <Slider2 />}
 			{activeSlider === 'slider3' && <Slider3 />}
 			{activeSlider === 'slider4' && <Slider4 />}
-			<div className='h-96 bg-four flex items-center justify-center text-xl font-semibold text-white'>
-				O firmie
-			</div>
-			<div className='h-[200vh]'>ss</div>
+			<About />
 		</>
 	);
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import slide1Image from '../assets/A1.png';
+import slide2Image from '../assets/K1.png';
+
+const About = () => {
+	return (
+		<section className='relative bg-primary py-16'>
+			<div className='max-w-wrapper mx-auto'>
+				<div className='flex flex-col lg:flex-row justify-center'>
+					<div className='relative w-full xl:w-1/2 lg:w-full px-8 ml-2 sm:ml-32 lg:ml-2'>
+						<div className='absolute left-0 top-0 h-56 w-4 bg-third rounded-t-full rounded-b-full'></div>
+						<div className='relative z-10 overflow-hidden'>
+							<img
+								src={slide1Image}
+								alt='Featured 1'
+								className='w-[470px] h-[516px] object-cover rounded-lg shadow-lg'
+							/>
+							<div className='absolute inset-0 bg-black opacity-10 rounded-lg w-[470px] h-full '></div>
+						</div>
+						<div className='absolute left-0 bottom-[-50px] z-10'>
+							<img
+								src={slide2Image}
+								alt='Featured 2'
+								className='w-[280px] lg:w[420] h-[295px] object-cover rounded-lg shadow-lg'
+							/>
+							<div className='absolute inset-0 bg-black opacity-10 rounded-lg w-[280px] lg:w[420] h-full '></div>
+						</div>
+					</div>
+
+					<div className='w-full xl:w-1/2 lg:w-full px-4 flex flex-col justify-center gap-3 mt-20 lg:mt-0'>
+						<div className='mb-4'>
+							<h2 className='text-6xl leading-tight uppercase font-teko font-light text-left'>
+								O nas
+								<span className='text-third'>.</span>
+							</h2>
+							<p className='text-2xl text-left text-third font-teko font-light uppercase'>
+								Od 2002 roku świadczymy usługi dla ponad 100 firm, realizując
+								670 projektów.
+							</p>
+						</div>
+
+						<p className='text-four text-left mb-4'>
+							Nasze kampanie reklamowe wygenerowały 108 120 000 wyświetleń i
+							pozyskały 15 768 000 potencjalnych klientów. Dzięki inwestycjom o
+							wartości 14 320 000 zł, przychody wyniosły 379 480 000 zł, co daje
+							średni ROI na poziomie +2650%.
+						</p>
+
+						<div className='flex flex-row items-center space-x-5 relative'>
+							<ul className='list-none space-y-2 w-[75%] text-left'>
+								<li className='flex items-start'>
+									<span className='text-third mr-2'>✔</span>
+									<span className='max-w-full break-words'>
+										Świadczymy usługi marketingu cyfrowego, obejmujące doradztwo
+										i zarządzanie.
+									</span>
+								</li>
+								<li className='flex items-start'>
+									<span className='text-third mr-2'>✔</span>
+									<span className='max-w-full break-words'>
+										Zajmujemy się różnymi taktykami i strategiami marketingu
+										internetowego.
+									</span>
+								</li>
+								<li className='flex items-start'>
+									<span className='text-third mr-2'>✔</span>
+									<span className='max-w-full break-words'>
+										Pomagamy w kampaniach marketingowych w mediach
+										społecznościowych.
+									</span>
+								</li>
+							</ul>
+
+							<div className='relative flex items-center justify-center h-full'>
+								<div className='w-1.5 h-full bg-third rounded-t-full rounded-b-full'></div>
+							</div>
+
+							<div className='flex items-center justify-center bg-four text-white rounded-full w-28 h-28 ml-4 lg:ml-6 flex-shrink-0'>
+								<div className='text-center font-teko font-semibold'>
+									<span className='text-lg block'>Since</span>
+									<span className='text-2xl'>2002</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
