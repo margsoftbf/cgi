d9c981f3c582ed3732d035546ddaecd13a348fb8
Marcin <mgarski@tutanota.com>
2024-10-06 13:56:15 +0200

    add another feat

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Typewriter from 'typewriter-effect';
-import dostarczamy from '../assets/dostarczamy.png';
-import pozyskujemy from '../assets/pozyskujemy.png';
-import generujemy from '../assets/generujemy.png';
-import zapewniamy from '../assets/zapewniamy.png';
+import slide1Image from '../assets/K.png';
+import slide2Image from '../assets/A.png';
+import slide3Image from '../assets/P.png';
 
 const Slider2 = () => {
 	const [activeSlide, setActiveSlide] = useState(0);
@@ -13,22 +12,22 @@ const Slider2 = () => {
 		{
 			title: 'Pozyskujemy klientów',
 			subtitle: 'z innowacyjną strategią marketingową!',
-			image: pozyskujemy,
+			image: slide1Image,
 		},
 		{
 			title: 'Dostarczamy ruch',
 			subtitle: 'dzięki efektywnej reklamie',
-			image: dostarczamy,
+			image: slide2Image,
 		},
 		{
 			title: 'Generujemy sprzedaż',
 			subtitle: 'dla każdej firmy',
-			image: generujemy,
+			image: slide3Image,
 		},
 		{
 			title: 'Zapewniamy zysk',
 			subtitle: 'dla każdej firmy',
-			image: zapewniamy,
+			image: slide1Image,
 		},
 	];
 
@@ -100,32 +99,52 @@ const Slider2 = () => {
 					Nasza strategia marketingowa pozwala na pozyskanie najbardziej
 					wartościowych klientów dla Twojego biznesu.
 				</motion.p>
-				<motion.div
-					className='flex items-center space-x-4'
-					variants={buttonVariants}
+
+				<motion.button
+					key={`button-${activeSlide}`}
+					className='px-6 py-3 bg-four text-white rounded-md font-semibold hover:bg-four/90 transition-colors relative z-10'
+					variants={slideVariants}
 					initial='initial'
 					animate='animate'
 					exit='exit'
 					transition={{ duration: 0.6, delay: 0.4 }}
 				>
-					<button className='px-6 py-3 bg-four text-white rounded-md font-semibold hover:bg-four/90 transition-colors relative z-10'>
-						Dowiedz się więcej
-					</button>
-				</motion.div>
+					Dowiedz się więcej
+				</motion.button>
 			</div>
 
 			<div className='order-1 md:order-2 w-full lg:w-1/2 h-full relative p-8 flex items-center justify-center'>
-				<motion.img
+				<motion.div
 					key={activeSlide}
-					src={slides[activeSlide].image}
-					alt='Slide'
-					className='w-48 h-48 lg:w-60 lg:h-60 object-contain'
+					className='absolute bg-white rounded-lg w-48 h-48 lg:w-96 lg:h-80'
 					variants={imageVariants}
 					initial='initial'
 					animate='animate'
 					exit='exit'
 					transition={{ duration: 1.8 }}
 				/>
+				<div className='order-1 md:order-2 w-full h-full relative p-8 flex items-center justify-center overflow-hidden'>
+					<motion.div
+						key={`shape-background-${activeSlide}`}
+						className='absolute bg-secondary rounded-md w-full h-full'
+						variants={imageVariants}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						transition={{ duration: 1.8 }}
+					/>
+					<motion.img
+						key={activeSlide}
+						src={slides[activeSlide].image}
+						alt='Slide'
+						className='w-48 h-48 lg:w-full lg:h-72 rounded-lg object-cover'
+						variants={imageVariants}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						transition={{ duration: 1.2 }}
+					/>
+				</div>
 			</div>
 		</section>
 	);
